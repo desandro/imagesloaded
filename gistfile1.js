46c382ab0e2195f8d25158f2097d01aa94157486
David DeSandro <david@desandro.com>
2011-01-26 13:08:48 -0500

    Change .filter -> .find.  So it can be used on parent element

--- gistfile1.js
+++ gistfile1.js
@@ -1,6 +1,12 @@
-// $('img.photo',this).imagesLoaded(myFunction)
-// execute a callback when all images have loaded.
+// $('#content-with-images').imagesLoaded( myFunction )
+// execute a callback when all images inside a parent have loaded.
 // needed because .load() doesn't work on cached images
+
+// Useful for Masonry or Isotope, triggering dynamic layout
+// after images have loaded:
+//    $('#content').imagesLoaded( function(){
+//      $('#content').masonry();
+//    });
 
 // mit license. paul irish. 2010.
 // webkit fix from Oren Solomianik. thx!
@@ -10,20 +16,23 @@
 
 
 $.fn.imagesLoaded = function(callback){
-  var elems = this.filter('img'),
-      len   = elems.length;
-      
+  var elems = this.find('img'),
+      len   = elems.length,
+      _this = this;
+
   elems.bind('load',function(){
-      if (--len <= 0){ callback.call(elems,this); }
+    if (--len <= 0){ 
+      callback.call( _this ); 
+    }
   }).each(function(){
-     // cached images don't fire load sometimes, so we reset src.
-     if (this.complete || this.complete === undefined){
-        var src = this.src;
-        // webkit hack from http://groups.google.com/group/jquery-dev/browse_thread/thread/eee6ab7b2da50e1f
-        // data uri bypasses webkit log warning (thx doug jones)
-        this.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
-        this.src = src;
-     }  
+    // cached images don't fire load sometimes, so we reset src.
+    if (this.complete || this.complete === undefined){
+      var src = this.src;
+      // webkit hack from http://groups.google.com/group/jquery-dev/browse_thread/thread/eee6ab7b2da50e1f
+      // data uri bypasses webkit log warning (thx doug jones)
+      this.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+      this.src = src;
+    }  
   }); 
 
   return this;
