68c5e8f49f9f1ef350fd9b08b88b4491debcbe8c
Tomas Sardyha <darsain@gmail.com>
2011-11-24 22:13:12 +0100

    Fix loaded images check with caching

--- jquery.imagesloaded.js
+++ jquery.imagesloaded.js
@@ -21,14 +21,17 @@
     var $this = this,
         $images = $this.find('img').add( $this.filter('img') ),
         len = $images.length,
-        blank = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
+        blank = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==',
+        loaded = [];
 
     function triggerCallback() {
       callback.call( $this, $images );
     }
 
     function imgLoaded( event ) {
-      if ( --len <= 0 && event.target.src !== blank ){
+      if ( event.target.src === blank || $.inArray( this, loaded ) != -1 ) return;
+      loaded.push(this);
+      if ( --len <= 0 ){
         setTimeout( triggerCallback );
         $images.unbind( 'load error', imgLoaded );
       }
@@ -40,13 +43,11 @@
 
     $images.bind( 'load error',  imgLoaded ).each( function() {
       // cached images don't fire load sometimes, so we reset src.
-      if (this.complete || typeof this.complete === "undefined"){
-        var src = this.src;
-        // webkit hack from http://groups.google.com/group/jquery-dev/browse_thread/thread/eee6ab7b2da50e1f
-        // data uri bypasses webkit log warning (thx doug jones)
-        this.src = blank;
-        this.src = src;
-      }
+      var src = this.src;
+      // webkit hack from http://groups.google.com/group/jquery-dev/browse_thread/thread/eee6ab7b2da50e1f
+      // data uri bypasses webkit log warning (thx doug jones)
+      this.src = blank;
+      this.src = src;
     });
 
     return $this;
