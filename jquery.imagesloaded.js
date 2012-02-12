06352b7a032a0b7b47359aca3066fee0f58d2835
Laurens Meurs <lmeurs@wiedes.nl>
2012-02-12 01:54:51 +0100

    added callback function on every single image load

--- jquery.imagesloaded.js
+++ jquery.imagesloaded.js
@@ -16,7 +16,7 @@ var BLANK = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAO
 // execute a callback when all images have loaded.
 // needed because .load() doesn't work on cached images
 
-// callback is executed when all images has fineshed loading
+// callback is executed when all images has finished loading
 // callback function arguments: $all_images, $proper_images, $broken_images
 // `this` is the jQuery wrapped container
 
@@ -25,15 +25,26 @@ var BLANK = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAO
 // fail method arguments: .fail( function( $all_images, $proper_images, $broken_images ){ ... } )
 // progress method arguments: .progress( function( images_count, loaded_count, proper_count, broken_count )
 
-$.fn.imagesLoaded = function( callback ) {
+// Alterations by Laurens Meurs (11 February 2012):
+// added extra function .imageLoaded( singleImageCallback , allImagesCallback) to:
+// * execute singleImageCallback() on every loaded image and 
+// * execute optional allImagesCallback() when all images are loaded (default imagesLoaded behaviour)
+
+// callback function arguments: $all_images, loaded_count, proper_count, broken_count
+// `this` is the image HTML element (like with events)
+$.fn.imageLoaded = function( singleImageCallback , allImagesCallback ) {
+	return this.imagesLoaded( allImagesCallback , singleImageCallback );
+}
+
+$.fn.imagesLoaded = function( allImagesCallback , singleImageCallback ) {
 	var $this = this,
 		deferred = $.isFunction($.Deferred) ? $.Deferred() : 0,
 		hasNotify = $.isFunction(deferred.notify),
 		$images = $this.find('img').add( $this.filter('img') ),
-		len = $images.length,
 		loaded = [],
 		proper = [],
-		broken = [];
+		broken = [],
+		counter = 0; // we need a separate counter for usage inside the timeout
 
 	function doneLoading() {
 		var $proper = $(proper),
@@ -47,42 +58,56 @@ $.fn.imagesLoaded = function( callback ) {
 			}
 		}
 
-		if ( $.isFunction( callback ) ) {
-			callback.call( $this, $images, $proper, $broken );
+		if ( $.isFunction( allImagesCallback ) ) {
+			allImagesCallback.call( $this, $images, $proper, $broken );
 		}
 	}
 
 	function imgLoaded( event ) {
+		var image = this; // make reference for usage inside timeout
+
 		// dont proceed if img src is blank or if img is already loaded
-		if ( event.target.src === BLANK || $.inArray( this, loaded ) !== -1 ) {
+		if ( event.target.src === BLANK || $.inArray( image, loaded ) !== -1 ) {
 			return;
 		}
 
 		// store element in loaded images array
-		loaded.push( this );
+		loaded.push( image );
 
 		// keep track of broken and properly loaded images
 		if ( event.type === 'error' ) {
-			broken.push( this );
+			broken.push( image );
 		} else {
-			proper.push( this );
+			proper.push( image );
 		}
 
 		// cache event type in element data for future calls
-		$.data( this, 'imagesLoaded', { event: event.type, src: this.src } );
+		$.data( image, 'imagesLoaded', { event: event.type, src: image.src } );
 
 		if ( hasNotify ) {
 			deferred.notify( $images.length, loaded.length, proper.length, broken.length );
 		}
 
-		if ( --len <= 0 ){
-			setTimeout( doneLoading );
+		if ( $images.length === loaded.length ){
 			$images.unbind( '.imagesLoaded', imgLoaded );
 		}
+
+		// call optional singleImageCallback
+		// define arguments for singleImageCallback, actual values might be changed by the time the timeout is executed
+		var arguments = [ $images, loaded.length, proper.length, broken.length ];
+		setTimeout( function() {
+			if ( $.isFunction( singleImageCallback ) ) {
+				singleImageCallback.apply( image, arguments );
+			}
+
+			if ( $images.length === ++counter ){
+				doneLoading();
+			}
+		});
 	}
 
 	// if no images, trigger immediately
-	if ( !len ) {
+	if ( $images.length === 0 ) {
 		doneLoading();
 	}
 
