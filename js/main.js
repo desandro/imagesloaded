26e74abf07a5ef99ff216e14e853e9030bbc536b
Tomas Sardyha <darsain@gmail.com>
2012-09-10 20:02:01 +0200

    Presentation update

--- js/main.js
+++ js/main.js
@@ -1,38 +1,5 @@
-$(function(){
-
-	// -----------------------------------------------------------------------------------
-	//   Page scripts
-	// -----------------------------------------------------------------------------------
-	(function(){
-
-		// Tabs navigation
-		var $tabs = $('#tabs').find('a'),
-			$container = $('#sections'),
-			$sections = $container.children();
-
-		$tabs.on('click', function(){
-
-			var id = '#'+$(this).data('activate');
-
-			$tabs.parent().removeClass('active');
-			$(this).parent().addClass('active');
-			$sections.addClass('hidden').filter(id).removeClass('hidden');
-
-			return false;
-
-		});
-
-		// Back to top button
-		$('a[href="#top"]').on('click', function(e){
-			e.preventDefault();
-			$(document).scrollTop(0);
-		});
-
-	})();
-
-	/* Trigger prettyPrint */
-	prettyPrint();
-
+jQuery(function($){
+'use strict';
 
 	// -----------------------------------------------------------------------------------
 	//   Simple example
@@ -74,12 +41,12 @@ $(function(){
 				case 'run':
 					//Empty holder and apped images
 					holder.empty().append(
-						'<img src="images/img1.jpg" alt="Lorempixel">' +
-						'<img src="images/img2.jpg" alt="Lorempixel">' +
-						'<img src="images/img3.jpg" alt="Lorempixel">' +
-						'<img src="images/img4.jpg" alt="Lorempixel">' +
-						'<img src="images/img5.jpg" alt="Lorempixel">' +
-						'<img src="images/img6.jpg" alt="Lorempixel">'
+						'<img src="img/img1.jpg" alt="Lorempixel">' +
+						'<img src="img/img2.jpg" alt="Lorempixel">' +
+						'<img src="img/img3.jpg" alt="Lorempixel">' +
+						'<img src="img/img4.jpg" alt="Lorempixel">' +
+						'<img src="img/img5.jpg" alt="Lorempixel">' +
+						'<img src="img/img6.jpg" alt="Lorempixel">'
 					);
 					runTest( holder );
 				break;
@@ -148,14 +115,14 @@ $(function(){
 			statusBar.hide();
 			progress.show();
 
-			// Call imagesLoaded with callback and save the deferred object
+			// Call imagesLoaded with callback, defer the determination, and save the deferred object
 			var dfd = holder.imagesLoaded(function( $images, $proper, $broken ){
 
 				totalLabel.text( $images.length );
 				properLabel.text( $proper.length );
 				brokenLabel.text( $broken.length );
 
-			});
+			}, true);
 
 			// Deferred magic
 			dfd.progress(function( isBroken, $images, $proper, $broken ){
@@ -174,13 +141,16 @@ $(function(){
 
 				var dfdState = dfd.state();
 
-				dfdLabel.addClass( dfdState === 'resolved' ? 'label-success' : 'label-important' ).text( dfdState.toUpperCase() );
+				dfdLabel.addClass( 'label-' + ( dfdState === 'resolved' ? 'success' : 'important' ) ).text( dfdState );
 
 				progress.hide();
 				statusBar.show();
 				progressBar.css({ width: 0 });
 
 			});
+
+			// Start the determination process
+			dfd.start();
 
 		}
 
@@ -236,6 +206,49 @@ $(function(){
 			});
 
 		});
+
+	})();
+
+
+	// -----------------------------------------------------------------------------------
+	//   Page scripts
+	// -----------------------------------------------------------------------------------
+	(function(){
+
+		// Navigation
+		var $nav = $('#nav'),
+			$sections = $('#sections').children(),
+			activeClass = 'active';
+
+		// Tabs
+		$nav.on('click', 'a', function(e){
+			e.preventDefault();
+			activate( $(this).attr('href').substr(1) );
+		});
+
+		// Back to top button
+		$('a[href="#top"]').on('click', function(e){
+			e.preventDefault();
+			$(document).scrollTop(0);
+		});
+
+		// Activate a section
+		function activate( sectionID, initial ){
+
+			sectionID = sectionID && $sections.filter('#'+sectionID).length ? sectionID : $sections.eq(0).attr('id');
+			$nav.find('a').removeClass(activeClass).filter('[href=#'+sectionID+']').addClass(activeClass);
+			$sections.hide().filter('#'+sectionID).show();
+
+			if( !initial ){
+				window.location.hash = '!' + sectionID;
+			}
+
+			$(document).trigger('activated', [ sectionID ] );
+
+		}
+
+		// Activate initial section
+		activate( window.location.hash.match(/^#!/) ? window.location.hash.substr(2) : 0, 1 );
 
 	})();
 
