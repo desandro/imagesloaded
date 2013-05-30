933078ba43b9203722919aa72182dca9a6a3e308
Matthew Tretter <m@tthewwithanm.com>
2013-05-29 20:14:57 -0400

    Use requirejs to create packaged version
    This ensures that the packaged file will work with AMD loaders.

--- tasks/bower-list-sources.js
+++ tasks/bower-list-sources.js
@@ -9,10 +9,24 @@ module.exports = function( grunt ) {
   'use strict';
 
   grunt.registerTask( 'bower-list-sources', function() {
+    var _ = grunt.util._;
     var done = this.async();
+    var jsRe = /\.js$/i
+
+    var extractPath = function(files) {
+      var path;
+      if (_.isString(files)) {
+        path = files;
+      } else {
+        path = _.filter(files, function(el) { return jsRe.test(el); })[0];
+      }
+      if (path) {
+        return path.replace(jsRe, '');
+      }
+    }
 
     // get map JSON from bower list --map
-    var childProc = spawn('bower', 'list --sources'.split(' ') );
+    var childProc = spawn('bower', 'list --map'.split(' ') );
     var sourcesSrc = '';
     childProc.stdout.setEncoding('utf8');
     childProc.stdout.on('data',  function( data ) {
@@ -21,23 +35,22 @@ module.exports = function( grunt ) {
 
     childProc.on('close', function() {
       var bowerSources = JSON.parse( sourcesSrc );
-      // set bowerMap
+      var paths = {};
 
-      // remove jquery, qunit, & EventEmitter.min.js
-      var bowerJsSources = bowerSources['.js'].filter( function( src ) {
-        var isMin = src.indexOf('.min.js') !== -1;
-        var isJquery = src.indexOf('jquery.js') !== -1;
-        var isQunit = src.indexOf('qunit.js') !== -1;
-        return !isMin && !isJquery && !isQunit;
+      // Build the paths dict from the bower config.
+      _.each(bowerSources, function(value, key) {
+        var path = extractPath(value.source.main || value.source.scripts);
+        if (path) {
+          paths[key] = path;
+        }
       });
-      // add bower JS to JS collection
-      var jsSrcs = grunt.config.get('concat.pkgd.src');
-      jsSrcs = bowerJsSources.concat( jsSrcs );
 
-      // set config so it gets concat and uglified
-      grunt.config.set( 'concat.pkgd.src', jsSrcs );
+      // Set config so requirejs can find the bower dependencies
+      grunt.config.set( 'requirejs.pkgd.options.paths', paths );
+
+      // Tell the uglify command what file to uglify.
       grunt.config.set( 'uglify.pkgd.files', {
-        'imagesloaded.pkgd.min.js': jsSrcs
+        'imagesloaded.pkgd.min.js': [grunt.config.get('requirejs.pkgd.options.out')]
       });
 
       done();
