915cb58b95876d2dbb08554b5548c728c66e4a8b
Michael Bester <michael@kimili.com>
2014-09-02 14:54:09 -0400

    Adding test for picture element

---
+++ test/picture-element.js
@@ -0,0 +1,10 @@
+test( 'picture-element', function() {
+  'use strict';
+
+  stop();
+  imagesLoaded('#picure-element', { debug: true }).on( 'done', function( obj ) {
+    ok( true, 'picture element image loaded' );
+    equal( obj.images.length, 1, 'instance has 1 images' );
+    start();
+  });
+});
