5e6ce4d042cfe4ba255e04c152b07351f12c132a
David DeSandro <desandrocodes@gmail.com>
2015-10-28 10:51:12 -0400

    check for multiple BG support for IE8

--- test/background.js
+++ test/background.js
@@ -1,7 +1,15 @@
 QUnit.test( 'background', function( assert ) {
   'use strict';
 
-  var done = assert.async( 9 );
+  // from Modernizr
+  var supportsMultiBGs = ( function() {
+    var style = document.createElement('a').style;
+    style.cssText = 'background:url(https://),url(https://),red url(https://)';
+    return (/(url\s*\(.*?){3}/).test(style.background);
+  })();
+
+  var multiBGCount = supportsMultiBGs ? 3 : 0;
+  var done = assert.async( 6 + multiBGCount );
 
   var imgLoad0 = imagesLoaded( '#background .tulip', { background: true }, function() {
     assert.ok( true, 'callback triggered on .orange-tree');
@@ -18,20 +26,22 @@ QUnit.test( 'background', function( assert ) {
   // multiple backgrounds
   var imgLoad2 = imagesLoaded( '#background .multi', { background: true }, function() {
     assert.ok( true, 'callback triggered on .multi');
-    assert.equal( imgLoad2.images.length, 3, '3 images on .images' );
+    assert.equal( imgLoad2.images.length, multiBGCount, 'correct multiple BG count on .images' );
     done();
   });
 
   // multiple elements
   var imgLoad3 = imagesLoaded( '#background .bg-box', { background: true }, function() {
     assert.ok( true, 'callback triggered on .bg-box');
-    assert.equal( imgLoad3.images.length, 5, '5 images on .bg-box' );
+    var count = 2 + multiBGCount;
+    assert.equal( imgLoad3.images.length, count, count + ' images on .bg-box' );
     done();
   });
 
   imgLoad3.on('progress', function( instance, image, element ) {
     assert.ok( true, 'progress on .bg-box; ' + image.img.src );
     assert.equal( image.isLoaded, true, 'image.isLoaded == true' );
+    assert.equal( element.nodeName, 'DIV', 'element is DIV, not IMG' );
     done();
   });
 
